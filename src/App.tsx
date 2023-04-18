572742a8a594954a62c3915bac09b00463d30b1e
jennifer-cruz <jennifermrcruz@gmail.com>
2023-04-18 08:10:02 -0300

    incluindo pagina de cadastro e cadastro de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import Footer from "./components/estaticos/footer/Footer";
 import Home from "./paginas/home/Home";
 import "./App.css";
 import Login from "./paginas/login/Login";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-
         <Route path="/login" element={<Login />} />
+        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
       </Routes>
       <Footer />
     </Router>
