cc8889e056bf111965220c3a8204182f75cebb39
jennifer-cruz <jennifermrcruz@gmail.com>
2023-04-20 15:26:47 -0300

    comunicação com backend - tema e postagem

--- src/components/temas/listaTemas/ListaTemas.tsx
+++ src/components/temas/listaTemas/ListaTemas.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
-import { Box } from '@mui/material';
+import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import {Box} from '@mui/material';
+import Temas from '../../../models/Temas';
 import './ListaTemas.css';
+import useLocalStorage from 'react-use-localstorage';
+import {useNavigate} from 'react-router-dom';
+import { busca } from '../../../services/Service';
 
 function ListaTemas() {
+  const [temass, setTemass] = useState<Temas[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let navigate = useNavigate();
+
+  useEffect(()=>{
+    if(token == ''){
+      alert("Você precisa estar logado")
+      navigate("/login")
+    }
+  }, [token])
+
+
+  async function getTemas(){
+    await busca("/tema", setTemass, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+
+  useEffect(()=>{
+    getTemas()
+  }, [temass.length])
+
   return (
     <>
+    {
+      temass.map(temass =>(
       <Box m={2} >
         <Card variant="outlined">
           <CardContent>
@@ -14,20 +45,20 @@ function ListaTemas() {
               Tema
             </Typography>
             <Typography variant="h5" component="h2">
-              Minha descrição
+             {temass.descricao}
             </Typography>
           </CardContent>
           <CardActions>
             <Box display="flex" justifyContent="center" mb={1.5} >
 
-              <Link to="" className="text-decorator-none">
+              <Link to={`/formularioTema/${temass.id}`} className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" className="marginLeft" size='small' color="primary" >
                     atualizar
                   </Button>
                 </Box>
               </Link>
-              <Link to="" className="text-decorator-none">
+              <Link to={`/deletarTema/${temass.id}`} className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" size='small' color="secondary">
                     deletar
@@ -38,9 +69,11 @@ function ListaTemas() {
           </CardActions>
         </Card>
       </Box>
-
+      ))
+      }
     </>
-  )
+  );
 }
+
 
 export default ListaTemas;
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,13 +3,17 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: 'https://blogpessoal-nktn.onrender.com'
 })
-
-export const cadastroUsuario = async (url: string, dados: Object, setDado: Function) => {
-    const resposta = await api.post(url, dados)
+export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
     setDado(resposta.data)
 }
 
-export const login = async (url: any, dados: any, setDado: any) => {
-    const resposta = await api.post(url, dados)
+export const login = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
     setDado(resposta.data.token)
+}
+
+export const busca = async(url: any,setDado: any, header: any) => { 
+    const resposta = await api.get(url,header)
+    setDado(resposta.data)
 }
